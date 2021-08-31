0ae39ef18e04c330106309d0fe79d9d23bb618b8
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-08-31 16:26:24 +0600

    JS Object Concepts

---
+++ summary.js
@@ -0,0 +1,15 @@
+const nayok = { name: 'hero', age: '28', isMarried: false };
+for (const property in nayok) {
+    console.log(property);
+}
+console.log(Object.entries(nayok));
+
+const obj = { a: 1, b: 7, c: 3, length: 2 };
+console.log(Object.keys(obj).length);
+
+const obj1 = { module: 35, video: 2 };
+const obj2 = { module: 35, video: 2 };
+console.log(obj1 === obj2);
+
+const getGirlFriend = (name = "chokina") => "name";
+console.log(getGirlFriend());
